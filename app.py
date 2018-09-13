9d9e7d619d6507a0d0d7d86a11a70c562ef5c8d8
Ensar Kovankaya <ensar@yolcu360.com>
2018-09-14 00:05:32 +0300

    Add more functions

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
+import re
+from urllib.parse import urlparse
+from multiprocessing.pool import ThreadPool
 
 logging.basicConfig(level='INFO')
 
@@ -58,7 +61,7 @@ def google_search(city, keyword, page_number):
         verified_links = []
 
         for link in raw_links:
-            verified_links.append(link.replace('/url?q=', ''))
+            verified_links.append(clean_site_url(link.replace('/url?q=', '')))
 
         logging.info("Links extracted for search {}.".format(search_term))
 
@@ -98,3 +101,101 @@ def download_site(site):
     driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
     driver.get(site)
     return driver.page_source
+
+
+def extract_emails(html):
+    """
+    Extract emails from html.
+
+    :param html: Html page
+    :return: List[str] Unique list of mails.
+    """
+
+    return list(set(re.findall('[\w\.-]+@[\w\.-]+\.[\w\.-]+', html)))
+
+
+def find_domain_mails(domain, mails):
+    """
+
+    :param str domain: web site domain. Ex: example.com
+    :param List[str] mails: Mail list
+    :return: List[str]
+    """
+
+    domain_mails = []
+    for mail in mails:
+        if mail.endswith(domain):
+            domain_mails.append(mail)
+    return domain_mails
+
+
+def generate_contact_urls(site):
+    """
+    Generates site urls for concat pages.
+
+    :param str site: Site url. Example: http://example.com
+    :return: [
+        "http://example.com/contact",
+    ]
+    """
+
+    patterns = [
+        "contact",
+        "contact-us",
+        "contactus",
+        "contact.html",
+        "contact.php",
+        "about",
+        "aboutus",
+        "about-us",
+        "about.html",
+        "about.php"
+    ]
+
+    links = []
+    for ptr in patterns:
+        url = "{site}/{pattern}".format(site=site, pattern=ptr)
+        links.append(url)
+    return links
+
+
+def clean_site_url(url):
+    """
+    Remove any query parameters from url
+
+    :param str url: Site url. Example: http://example.com/?a=b&asdad?
+    :return: http://example.com
+    """
+    return '{uri.scheme}://{uri.netloc}/'.format(uri=urlparse(url))
+
+
+def process_site(site_url):
+    """
+    Download site and sub page of the sites than extract emails.
+
+    :param str site_url: Site url
+    :return: List[str]
+    """
+
+    pages = [site_url]
+    contact_urls = generate_contact_urls(site_url)
+    pages.extend(contact_urls)
+
+
+def main(city, keyword, start_page=1, end_page=20):
+    """
+
+    :param city: Search city
+    :param keyword: Search keyword
+    :return:
+    """
+
+    web_site_links = []
+
+    for i in range(start_page, end_page):
+        result = google_search(city, keyword, i)
+        if result.get('link_count') > 0:
+            web_site_links.extend(result.get('verified_links'))
+
+    for site in web_site_links:
+        process_site(site)
